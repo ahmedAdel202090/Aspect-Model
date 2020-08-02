ecb7570a5254697573ba88e4215c11c107fcd96a
ahmedAdel202090 <ahmed.adel202090@gmail.com>
2020-08-02 20:08:21 +0200

    remove threading

--- app/aspect/aspect_knowlege_base.py
+++ app/aspect/aspect_knowlege_base.py
@@ -1,36 +1,7 @@
 from app.aspect.utils import seq_to_vec
 from app.aspect.aspect_extraction import AspectExtraction
-from threading import Thread
 
-MAX_SIZE_THREAD_GET = 5
-def partition(arr, num_threads):
-    parts = []
-    r = len(arr) / num_threads # r represent ratio between part size for each thread
-    for i in range(num_threads):
-        st = int(i*r)
-        ed = int((i+1)*r)
-        part = arr[st:ed]
-        parts.append(part)
-    return parts
-def task(extractor,doc_vec,texts,ngram,skip_keywords,result_aspects):
-      for text in texts:
-          aspects = extractor(doc_vec,text,ngram,skip_keywords)
-          result_aspects.extend(aspects)
-def assign_threads(parts,results,extractor,doc_vec,ngram,skip_keywords):
-  threads = []    
-  for part in parts:
-    thread = Thread(target=task, args=(extractor,doc_vec,part,ngram,skip_keywords,results))
-    thread.start()
-    threads.append(thread)
-  for thread in threads:
-    thread.join()  
-def get_num_threads(size):
-  if size<2:
-    return 1  
-  for num_threads in range(2,size):
-    if ((size*1.0)/num_threads) < MAX_SIZE_THREAD_GET:
-      return num_threads
-  return size         
+
 class AspectKnowledgeBase(object):
   def __init__(self):
     self.texts = []
@@ -41,12 +12,11 @@ class AspectKnowledgeBase(object):
     self.texts = texts
     text = ' '.join(texts)
     doc_vec = seq_to_vec(text)
-    num_threads = get_num_threads(len(texts))
-    print('num_threads............................',num_threads)
-    texts_partitions = partition(texts,num_threads)
-    aspects = []
-    assign_threads(texts_partitions,aspects, self.aspect_extraction,doc_vec,ngram,skip_keywords)
-    for aspect_obj in aspects:
+    all_aspects = []
+    for text in texts:
+      aspects = self.aspect_extraction(doc_vec,text,ngram,skip_keywords)
+      all_aspects.extend(aspects)
+    for aspect_obj in all_aspects:
       self.num_keywords +=1
       if not aspect_obj['aspect'] in self.knowledge_set.keys():
         self.knowledge_set[aspect_obj['aspect']] = {'sentiment':[0,0,0,0,0] , 'score':aspect_obj['score']}
