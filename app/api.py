c36fc4ffe2a24b9a7f033fd500a6d97b5d4bb585
ahmedAdel202090 <ahmed.adel202090@gmail.com>
2020-08-06 17:38:34 +0200

    handle callback

--- app/api.py
+++ app/api.py
@@ -4,17 +4,32 @@ import threading
 import requests
 from app.aspect.aspect_extraction import AspectExtraction
 from app.aspect.aspect_knowlege_base import AspectKnowledgeBase
-from app.aspect.utils import seq_to_vec,sentence_segmenter
+from app.aspect.utils import seq_to_vec, sentence_segmenter
 import threading
 import os
 import time
 import gensim
 from gensim.models import Word2Vec, KeyedVectors
 import numpy as np
-
+MAX_REQUEST_RETRIES = 5
 is_loaded = False
 MODEL_URL = 'https://storagerat.blob.core.windows.net/telda/word2vec-150-400k.bin?sp=r&st=2020-07-29T22:12:06Z&se=2020-08-30T06:12:06Z&spr=https&sv=2019-12-12&sr=b&sig=l7lQ8gSusC81xeobOP6rzTbARjvq89pSIYCj2uuvYfo%3D'
 model = None
+
+
+def callback(target_fun,input_data,callback_url,call_name):
+    print("[{0}] Request is in progress and will be delivered to {1}".format(call_name,callback_url))
+    result = target_fun(input_data)
+    resp = {
+        "data":result,
+        "msg":"Succeed",
+        "errmsg":"NULL"
+    }
+    for _ in range(MAX_REQUEST_RETRIES):
+        webhook_request = requests.post(url=callback_url, json=resp)
+        if webhook_request.status_code == 200:
+            print("[{0}]:Request Delivered to <{1}>.".format(call_name, callback_url))
+            break
 
 
 def load_model():
@@ -33,47 +48,60 @@ def load_model():
 def load_word2vec():
     global model
     global is_loaded
-    if model!=None and is_loaded:
-        return jsonify({'loaded':True , 'msg':'model is loaded...'})
+    if model != None and is_loaded:
+        return jsonify({'loaded': True, 'msg': 'model is loaded...'})
     else:
-        threading.Thread(target=load_model, name="Model-Loading-Thread").start()
-        return jsonify({'loaded':False , 'msg':'model not loaded'})
+        threading.Thread(target=load_model,
+                         name="Model-Loading-Thread").start()
+        return jsonify({'loaded': False, 'msg': 'model not loaded'})
 
-@app.route('/sentence-seg',methods=['POST'])
+
+@app.route('/sentence-seg', methods=['POST'])
 def sentence_segmentation():
     reviews = request.json["reviews"]
-    #callback = request.json["callback"]
-    result = sentence_segmenter(reviews)
-    return jsonify(result)
+    callback_url = request.json["callback_url"]
+    resp=None
+    if reviews is None or callback_url is None:
+        resp = {
+            "data": "NULL",
+            "msg": "Prediction Failed",
+            "errMsg": "One of the required parameters is null"
+        }
+    else:
+        threading.Thread(target=callback, args=(sentence_segmenter,reviews,callback_url,"Sentence-Segmentation")).start()
+        resp = {
+            "msg":"Sentence-Segmentation starting",
+            "errmsg":"NULL"
+        }    
+    return jsonify(resp)
 
-@app.route('/api/aspect_extraction',methods=["POST"])
+
+@app.route('/api/aspect_extraction', methods=["POST"])
 def aspect_extraction():
     global model
     global is_loaded
-    if model!=None and is_loaded:
+    if model != None and is_loaded:
         review = request.json["review"]
         n_grams = request.json['n_grams']
         domains = request.json["tags"]
         extractor = AspectExtraction()
-        doc_vec = seq_to_vec(model,review)
-        result = extractor(model,doc_vec,review,int(n_grams),domains)
+        doc_vec = seq_to_vec(model, review)
+        result = extractor(model, doc_vec, review, int(n_grams), domains)
         return jsonify(result)
     else:
-         return jsonify({'loaded':False , 'msg':'model not loaded'})   
+        return jsonify({'loaded': False, 'msg': 'model not loaded'})
 
 
-@app.route('/api/create_knowledge_base',methods=["POST"])
+@app.route('/api/create_knowledge_base', methods=["POST"])
 def create_knowledge_base():
     global model
     global is_loaded
-    if model!=None and is_loaded:
+    if model != None and is_loaded:
         reviews = request.json["reviews"]
         n_grams = request.json['n_grams']
         domains = request.json["tags"]
         aspect_knowledge_base = AspectKnowledgeBase()
-        result = aspect_knowledge_base(model,reviews,n_grams,domains)
+        result = aspect_knowledge_base(model, reviews, n_grams, domains)
         return jsonify(result)
     else:
-        return jsonify({'loaded':False , 'msg':'model not loaded'})    
-
-
+        return jsonify({'loaded': False, 'msg': 'model not loaded'})
