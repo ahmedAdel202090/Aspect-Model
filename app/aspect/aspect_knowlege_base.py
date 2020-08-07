2f90a7fe708a6308460c176a43ac005b7aa147bc
ahmedAdel202090 <ahmed.adel202090@gmail.com>
2020-08-07 14:42:06 +0200

    handle callback in aspect

--- app/aspect/aspect_knowlege_base.py
+++ app/aspect/aspect_knowlege_base.py
@@ -10,7 +10,8 @@ class AspectKnowledgeBase(object):
     self.num_keywords=0
   def __call__(self,model,texts,ngram,skip_keywords):
     self.texts = texts
-    text = ' '.join(texts)
+    text_value = list(map(lambda x:x["text"],texts))
+    text = ' '.join(text_value)
     doc_vec = seq_to_vec(model,text)
     for text in texts:
       aspects = self.aspect_extraction(model,doc_vec,text,ngram,skip_keywords)
@@ -21,9 +22,17 @@ class AspectKnowledgeBase(object):
         sentiment = aspect_obj['sentiment']  
         self.knowledge_set[aspect_obj['aspect']]['sentiment'][sentiment] +=1
     self.rank_normalize()
-    self.knowledge_set = dict(sorted(self.knowledge_set.items(), key=lambda item: item[1]['score'],reverse=True))
+    #self.knowledge_set = dict(sorted(self.knowledge_set.items(), key=lambda item: item[1]['score'],reverse=True))
+    self.knowledge_set = self.transform_aspect_to_obj(self.knowledge_set)
     return self.knowledge_set    
   def rank_normalize(self):
     for keyword in self.knowledge_set.keys():
       keyword_freq = sum(self.knowledge_set[keyword]['sentiment'])
-      self.knowledge_set[keyword]['score'] = self.knowledge_set[keyword]['score'] * (keyword_freq / self.num_keywords)
+      self.knowledge_set[keyword]['score'] = self.knowledge_set[keyword]['score'] * (keyword_freq / self.num_keywords)  
+  def transform_aspect_to_obj(self,knowledge_set):
+    temp_knowledge_set = []
+    for aspect,value in knowledge_set.items():
+          value['aspect'] = aspect
+          temp_knowledge_set.append(value)
+    return temp_knowledge_set      
+
